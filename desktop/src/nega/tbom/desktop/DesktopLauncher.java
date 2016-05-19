addd008a002e24a556ea4efac9ef9f41ac95b19c
maxlep <mlepkows@stevens.edu>
2016-05-19 17:47:51 -0400

    Initial project upload.

---
+++ desktop/src/nega/tbom/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nega.tbom.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nega.tbom.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
