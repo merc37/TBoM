357521526b8509379607880f00a3f001f5ea3723
maxlep <mlepkows@stevens.edu>
2016-05-23 17:20:08 -0400

    Rebuilt project with "Tools" addon.

--- core/src/nega/tbom/MainGame.java
+++ core/src/nega/tbom/MainGame.java
@@ -1,31 +1,27 @@
 package nega.tbom;
 
-import nega.tbom.screens.GameScreen;
-import nega.tbom.screens.MainMenuScreen;
-import nega.tbom.screens.PauseScreen;
-
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MainGame extends Game {
-	
-	private Screen MENU, GAME, PAUSE;
+public class MainGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
 	
 	@Override
 	public void create () {
-		MENU = new MainMenuScreen(this);
-		GAME = new GameScreen(this);
-		PAUSE = new PauseScreen(this);
-		
-		this.setScreen(GAME);
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		super.render();
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
 	}
 }
--- core/src/nega/tbom/screens/MainMenuScreen.java
+++ core/src/nega/tbom/screens/MainMenuScreen.java
@@ -3,6 +3,8 @@ package nega.tbom.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import nega.tbom.MainGame;
 
